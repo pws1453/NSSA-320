27433efeeac25be970eafa94095452694f622b0f
Preston <pws1453@rit.edu>
2022-11-21 19:22:13 -0500

    lol nee to updot

--- Set-StaticIP.ps1
+++ Set-StaticIP.ps1
@@ -1,2 +1,3 @@
 Set-NetIPInterface -InterfaceAlias Ethernet -Dhcp Disabled
 New-NetIPAddress -InterfaceAlias Ethernet -IPAddress 10.0.3.104 -PrefixLength 24 -DefaultGateway 10.0.3.254
+Set-DnsClientServerAddress -InterfaceAlias Ethernet -ServerAddresses ("127.0.0.1","8.8.8.8")
