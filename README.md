e77a27bc9f03b011f344dbb4bf5cd3783b8dd669
Preston <pws1453@rit.edu>
2022-11-22 13:31:56 -0500

    updoots

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Ansible and Terraform Lab Setup
 Be sure that OpenSSH is enabled on ur windoze box!
 
+Windows needs to have a static IP and SSH Server setup.
+Denote this in your DHCP Setup.
+
 Make sure that the following is installed
 * Ansible
 * Ansible-Lint
--- run.sh
+++ run.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-# DHCP Templating <3
-jinja2 jinja_templates/Set-DHCPServer.j2 dhcp.config.yaml >> ./playbooks/scripts/DHCPServer-Setup.ps1
+
+# WINDOWS SERVER SETUP
 # Setup winrm on host
 ansible-playbook -vvv playbooks/win-init.yml -i config/inventory.yml 
 # Rename Windows Server box
 ansible-playbook -vvv playbooks/rename-winserve.yaml -i config/inventory.yml
-# Setup domain controller
-ansible-playbook -vvv playbooks/ad-domain.yml -i config/inventory.yml -e @config/ad_domains.config.yaml
+# Setup AD Domain
+ansible-playbook -vvv playbooks/ad-domain.yml -i config/inventory.yml -e @config/ad_domain.config.yaml
+# AD User Templating
+jinja2 jinja_templates/ad-users.j2 config/ad_domain.config.yaml >> playbooks/ad-users.yml
+
+# DHCP Templating <3
+jinja2 jinja_templates/Set-DHCPServer.j2 ./config/dhcp.config.yaml >> ./scripts/DHCPServer-Setup.ps1
+# Setup DHCP
+ansible-playbook -vvv playbooks/win-dhcp.yml -i config/inventory.yml 
