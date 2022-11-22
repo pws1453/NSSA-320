d54f4d4bb4fdb66ec89bfeaa9bb4fc1a7da08165
Preston <pws1453@rit.edu>
2022-11-21 23:33:22 -0500

    YUUUGE progress

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Ansible and Terraform Lab Setup
+Be sure that OpenSSH is enabled on ur windoze box!
+
+Make sure that the following is installed
+* Ansible
+* Ansible-Lint
+* Python3 (and pip)
+
+Example ansible run:
+```
+ansible-playbook -vvv playbooks/rename-winserve.yaml -i config/inventory.yml
+```
--- run.sh
+++ run.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+# DHCP Templating <3
 jinja2 jinja_templates/Set-DHCPServer.j2 dhcp.config.yaml >> ./playbooks/scripts/DHCPServer-Setup.ps1
+# Setup winrm on host
+ansible-playbook -vvv playbooks/win-init.yml -i config/inventory.yml 
+# Rename Windows Server box
+ansible-playbook -vvv playbooks/rename-winserve.yaml -i config/inventory.yml
+# Setup domain controller
+ansible-playbook -vvv playbooks/ad-domain.yml -i config/inventory.yml -e @config/ad_domains.config.yaml
