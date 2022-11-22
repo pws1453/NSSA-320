e70771db9e8926c2b5237a195039c3533687cfb6
Preston <pws1453@rit.edu>
2022-11-21 19:26:55 -0500

    updated static IP script

--- Set-StaticIP.ps1
+++ Set-StaticIP.ps1
@@ -1,3 +1,15 @@
-Set-NetIPInterface -InterfaceAlias Ethernet -Dhcp Disabled
-New-NetIPAddress -InterfaceAlias Ethernet -IPAddress 10.0.3.104 -PrefixLength 24 -DefaultGateway 10.0.3.254
-Set-DnsClientServerAddress -InterfaceAlias Ethernet -ServerAddresses ("127.0.0.1","8.8.8.8")
+$validarg = $args.count -eq 3
+if ($validarg)
+{
+    $ip = $args[0]
+    $prefix = $args[1]
+    $gateway = $args[2]
+    Set-NetIPInterface -InterfaceAlias Ethernet -Dhcp Disabled
+    New-NetIPAddress -InterfaceAlias Ethernet -IPAddress $ip -PrefixLength $prefix -DefaultGateway $gateway
+    Set-DnsClientServerAddress -InterfaceAlias Ethernet -ServerAddresses ("127.0.0.1","8.8.8.8")
+} else {
+    Write-Host "ERROR: Invalid Number of Arguments"
+    Write-Host "SYNTAX=================================================="
+    Write-Host "Set-StaticIP.ps1 [ip_address] [net_prefix] [def_gateway]"
+    throw "SYNTAX ERROR"
+}
